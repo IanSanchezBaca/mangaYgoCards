2f8da3c52b156f3c6edecd8cbf06cd49e4053253
IanSanchezBaca <iansanchez066@gmail.com>
2025-05-28 01:17:52 -0700

    added comments

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
+##################################################
 ### The main page this will prob not handle much
+##################################################
+
+import sys ### using this for argv
+from talkers import filetalker, webtalker
 
 def main():
-    print("Hello World!")
+    # if len(sys.argv) < 2:
+    #     print("Usage: ./main.py <ydk file>")
+    #     exit(-1)
+    # ydk = sys.argv[1]
+    # filetalker.openFile(ydk)
 
+
+    ydkdeck = filetalker.openFile("template/Goblins.ydk")
+    # ydkdeck = filetalker.openFile("template/link.ydk")
+    # ydkdeck = filetalker.openFile("template/altart.ydk")
+
+
+    deck = []
+    for card in ydkdeck:
+        deck.append(webtalker.searchCard(card))
+
+    
+
+    # if deck:
+    #     print(deck)
+
+    
 
 if __name__ == "__main__":
     main()
