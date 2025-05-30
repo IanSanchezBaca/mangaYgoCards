b179aae1aa212eab8c69949cef1557cabdc05db6
IanSanchezBaca <iansanchez066@gmail.com>
2025-05-30 03:05:52 -0700

    added stuff to see if it works as intended

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
 ##################################################
 ### The main page this will prob not handle much
 ##################################################
-
 import sys ### using this for argv
 import ast
-from talkers import filetalker, webtalker, imagetalker
+from talkers import filetalker, webtalker, imagetalker, databasetalker
 
 def main():
-    # if len(sys.argv) < 2:
-    #     print("Usage: ./main.py <ydk file>")
-    #     exit(-1)
-    # ydk = sys.argv[1]
-    # filetalker.openFile(ydk)
+    if len(sys.argv) < 2:
+        print("Usage: ./main.py <path to ydk file> <path to folder of images>")
+        exit(-1)
+    
+    ### loads the database
+    database = databasetalker.loadDataBase()
+    
+    ### grabs the ydk path and makes a deck of the codes
+    ydk = sys.argv[1] ### grab the ydk file path
+    ydkdeck = filetalker.openFile(ydk)
+
+
+    ### this checks if they added the path to the images
+    if len(sys.argv) >= 3:
+        imagetalker.changePath(sys.argv[2])
 
     # ydkdeck = filetalker.openFile("template/3cards.ydk")
     # ydkdeck = filetalker.openFile("template/Goblins.ydk")
     # ydkdeck = filetalker.openFile("template/link.ydk")
     # ydkdeck = filetalker.openFile("template/altart.ydk")
-
-    deck = testing()
-    imagetalker.changePath("bogus")
-    # deck = []
-    # for card in ydkdeck:
-    #     deck.append(webtalker.searchCard(card))
+    # deck = testing()
+    
+    new = []
+    deck = []
+    for card in ydkdeck:
+        check = databasetalker.check(database, card[0])
+        # databasetalker.check(database, card[0])
+        if check:
+            # print(check)
+            deck.append(check)
+        else:
+            temp = webtalker.searchCard(card[0])
+            if temp:
+                deck.append(temp)
+                new.append(temp)
 
 
     for card in deck:
-        # print(card)
         imagetalker.makeCard(card)
-
-    # print("END")
+        # print(card)
+        
+    
+    if len(new):
+        print("saving new cards!")
+        databasetalker.saveDataBase(new)
+    # else:
+    #     print("no new cards")
         
 
 
